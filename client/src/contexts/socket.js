a441d158d67caa39f8f4617d9b65fa22b20e4871
marine-bre <marine_bretonniere@hotmail.co.uk>
2020-11-25 15:32:07 +0200

    endpoint fix

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import io from 'socket.io-client'
 
 // const ENDPOINT = 'http://localhost:5000/'
-const ENDPOINT = 'https://movie-finder-app-react.herokuapp.com/'
+const ENDPOINT = 'https://movie-finder-app-react.herokuapp.com/socket.io/?EIO=4&transport=websocket'
 export const socket = io(ENDPOINT);
 
 export const SocketListen = ({preferences, setPreferences}) => {
